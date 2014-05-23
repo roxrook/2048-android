aa311b5ccdfe0eb257136253b402b688b594f437
Chan Nguyen <atbl1511@gmail.com>
2014-05-22 17:27:39 -0700

    Add layout for Nexus 7.

--- src/chan/android/game2048/MatrixView.java
+++ src/chan/android/game2048/MatrixView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -119,7 +118,6 @@ public class MatrixView extends TableLayout implements Swiper {
                 } else {
                     tiles[r][c].setText(Integer.toString(number));
                 }
-                tiles[r][c].setTextSize(TypedValue.COMPLEX_UNIT_SP, getTextSize(number));
                 tiles[r][c].setBackground(getResources().getDrawable(getDrawableId(number)));
 
                 if (matrix.isMergeSpot(r, c)) {
